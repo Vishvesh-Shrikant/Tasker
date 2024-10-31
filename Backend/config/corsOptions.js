4919825ed3a61d4b7c4132738b59c25a20a9a23e
Vishvesh-Shrikant <vishveshshri@gmail.com>
2024-10-31 23:22:57 +0530

    .

--- Backend/config/corsOptions.js
+++ Backend/config/corsOptions.js
@@ -1,7 +1,7 @@
-const allowedOrigins=['http://localhost:5173', 'https://tasker-client-vishvesh.vercel.app/']
+const allowedOrigins=['http://localhost:5173', 'https://tasker-client-vishvesh.vercel.app']
 
 const corsOptions={
-    origin: allowedOrigins, 
+    origin:allowedOrigins, 
     credentials: true,
 }  
 module.exports= corsOptions;
