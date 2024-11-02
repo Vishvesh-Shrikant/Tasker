842e56aa29424a742d9ce410081a356f0153dfa0
Vishvesh-Shrikant <vishveshshri@gmail.com>
2024-11-02 22:38:52 +0530

    .

--- Backend/config/allowedOrigins.js
+++ Backend/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins= [
-    'http://localhost:5173', 
-    'https://tasker-client-vishvesh.vercel.app'
+    "http://localhost:5173/", 
+    "ttps://tasker-client-vishvesh.vercel.app/"
 ]
 
 module.exports=allowedOrigins
