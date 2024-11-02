3d5f7537f8f698e58ece6b7ad73571b2474d7637
Vishvesh-Shrikant <vishveshshri@gmail.com>
2024-11-02 19:06:27 +0530

    cors update 3

--- Backend/config/corsOptions.js
+++ Backend/config/corsOptions.js
@@ -8,7 +8,7 @@ const corsOptions = {
             callback(new Error('Not allowed by CORS'));
         }
     },
-    methods: 'GET,POST,PUT,DELETE,PATCH',
+    methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization',
     optionsSuccessStatus: 200
 }
